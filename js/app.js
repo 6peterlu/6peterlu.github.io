158ba9559e46aeb89b0fe8ad698742788af86056
Peter Lu <6peterlu@gmail.com>
2017-07-28 15:58:10 -0700

    aesthetic fixes, plus first page contents

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ class App {
 		this.switchToHello = this.switchToHello.bind(this);
 		this.switchToWork = this.switchToWork.bind(this);
 		this.switchToNotWork = this.switchToNotWork.bind(this);
+		this.checkKeyboardInput = this.checkKeyboardInput.bind(this);
+		this.workSpanMouseover = this.workSpanMouseover.bind(this);
+		this.workSpanMouseout = this.workSpanMouseout.bind(this);
+		this.notWorkSpanMouseover = this.notWorkSpanMouseover.bind(this);
+		this.notWorkSpanMouseout = this.notWorkSpanMouseout.bind(this);
 
 		this.helloButton = document.querySelector("#tab-hello");
 		this.workButton = document.querySelector("#tab-work");
@@ -17,20 +22,58 @@ class App {
 		workButton.addEventListener("click", this.switchToWork);
 		const notWorkButton = document.querySelector("#tab-not-work");
 		notWorkButton.addEventListener("click", this.switchToNotWork);
+
+		const workSpan = document.querySelector("#span-work");
+		workSpan.addEventListener("mouseenter", this.workSpanMouseover);
+		workSpan.addEventListener("mouseout", this.workSpanMouseout);
+
+		const notWorkSpan = document.querySelector("#span-not-work");
+		notWorkSpan.addEventListener("mouseenter", this.notWorkSpanMouseover);
+		notWorkSpan.addEventListener("mouseout", this.notWorkSpanMouseout);
+
+		document.addEventListener('keypress', this.checkKeyboardInput);
 	}
 	switchToHello() {
 		this.helloPanel.classList.remove('inactive');
+		this.helloButton.classList.add('tab-hello-active');
 		this.workPanel.classList.add('inactive');
+		this.workButton.classList.remove('tab-work-active');
 		this.notWorkPanel.classList.add('inactive');
+		this.notWorkButton.classList.remove('tab-not-work-active');
 	}
 	switchToWork() {
 		this.workPanel.classList.remove('inactive');
+		this.workButton.classList.add('tab-work-active');
 		this.helloPanel.classList.add('inactive');
+		this.helloButton.classList.remove('tab-hello-active');
 		this.notWorkPanel.classList.add('inactive');
+		this.notWorkButton.classList.remove('tab-not-work-active');
 	}
 	switchToNotWork() {
 		this.notWorkPanel.classList.remove('inactive');
+		this.notWorkButton.classList.add('tab-not-work-active');
 		this.helloPanel.classList.add('inactive');
+		this.helloButton.classList.remove('tab-hello-active');
 		this.workPanel.classList.add('inactive');
+		this.workButton.classList.remove('tab-work-active');
+	}
+	checkKeyboardInput(event) {
+		switch(event.key) {
+			case "1": this.switchToHello(); break;
+			case "2": this.switchToWork(); break;
+			case "3": this.switchToNotWork(); break;
+		}
+	}
+	workSpanMouseover() {
+		this.workButton.classList.add('tab-work-active');
+	}
+	workSpanMouseout() {
+		this.workButton.classList.remove('tab-work-active');
+	}
+	notWorkSpanMouseover() {
+		this.notWorkButton.classList.add('tab-not-work-active');
+	}
+	notWorkSpanMouseout() {
+		this.notWorkButton.classList.remove('tab-not-work-active');
 	}
 }
